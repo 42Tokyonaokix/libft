d283b339a866d017fbf8c54d3f138058dd8c08de
naoki <ai.business.naokix@gmail.com>
2025-10-06 23:08:46 +0900

    ver159

--- srcs/ft_putnbr_fd.c
+++ srcs/ft_putnbr_fd.c
@@ -2,16 +2,13 @@
 
 void ft_putnbr_fd(int n, int fd)
 {
-	char	*itoa;
-
-	itoa = ft_itoa(n);
-	ft_putstr_fd(itoa, fd);
+	ft_putstr_fd(ft_itoa(n), fd);
 }
 
 //int main(void)
 //{
 //	int	fd;
-	
+
 //	fd = open("test1", O_WRONLY);
 //	ft_putnbr_fd(12345,fd);
 //}
